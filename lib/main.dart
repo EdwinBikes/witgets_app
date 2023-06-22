583168e7d95515e7defad091827f75af4365d6e4
Edwin bikes <edwinbykes@hotmail.com>
2023-06-22 18:44:08 -0500

    home screen creado

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/menu/menu_items.dart';
 
 void main() => runApp(const MyApp());
 
@@ -16,10 +17,47 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+        body: const _HomeView(),
       ),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (BuildContext context, int index) {
+        // return ListTile(title: Text(appMenuItems[0].title),); ………este fue el resultado de la tarea que dejo fernando en el curso
+        // final menuItem = appMenuItems[index];  …………… esto fue lo que el sugirio pero le falta el listtitle
+        // return Text(menuItem.title);
+        // resumen: me habia faltado crear una variable para implementar el appMenuItems para saber el index
+        final menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_back_ios_rounded, color: colors.primary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: (){},
     );
   }
 }
